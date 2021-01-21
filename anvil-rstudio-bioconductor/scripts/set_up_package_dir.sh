2c3606f72017575cc7a586ae81e0aaf57ceb7bc8
Gabriela <gcarrill@broadinstitute.org>
2021-01-21 12:15:18 -0500

    final pass

--- anvil-rstudio-bioconductor/scripts/set_up_package_dir.sh
+++ anvil-rstudio-bioconductor/scripts/set_up_package_dir.sh
@@ -10,7 +10,6 @@ R_PATH=`R CMD printenv R_LIBS_USER | sed 's/~\///g'`
 BIOCONDUCTOR_VERSION=`printenv BIOCONDUCTOR_DOCKER_VERSION | sed 's/\(^[0-9].[0-9][0-9]\).*/\1/g'`
 R_PACKAGE_DIR=${RSTUDIO_USER_HOME}/${R_PATH}-${BIOCONDUCTOR_VERSION}
 
-mkdir -p ${R_PACKAGE_DIR}
-chmod a+rwx ${R_PACKAGE_DIR}
+sudo -E -u rstudio mkdir -p ${R_PACKAGE_DIR}
 
 echo R_LIBS=${R_PACKAGE_DIR} >> /usr/local/lib/R/etc/Renviron.site
