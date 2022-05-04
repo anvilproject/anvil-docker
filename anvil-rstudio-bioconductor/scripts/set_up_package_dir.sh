262881e87446fed715c89e31e0ae5fd181cc0dea
Nitesh Turaga <nitesh.turaga@gmail.com>
2022-05-04 17:37:54 -0400

    remove the "/" to fix bug

--- anvil-rstudio-bioconductor/scripts/set_up_package_dir.sh
+++ anvil-rstudio-bioconductor/scripts/set_up_package_dir.sh
@@ -9,7 +9,7 @@ RSTUDIO_USER_HOME=/home/rstudio
 R_PATH=`Rscript -e "cat(Sys.getenv('R_LIBS_USER'))"`
 # R_PATH=`R CMD printenv R_LIBS_USER | sed 's/~\///g'`
 BIOCONDUCTOR_VERSION=`printenv BIOCONDUCTOR_DOCKER_VERSION | sed 's/\(^[0-9].[0-9][0-9]\).*/\1/g'`
-R_PACKAGE_DIR=${RSTUDIO_USER_HOME}/${R_PATH}-${BIOCONDUCTOR_VERSION}
+R_PACKAGE_DIR=${RSTUDIO_USER_HOME}${R_PATH}-${BIOCONDUCTOR_VERSION}
 
 sudo -E -u rstudio mkdir -p ${R_PACKAGE_DIR}
 
